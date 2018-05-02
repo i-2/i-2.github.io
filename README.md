1592c4df0ce4c867e44ffa6e9a85b3c73c93a77b
sathyanarrayanan <sathyanarrayanan1117@gmail.com>
2018-05-02 17:20:22 +0800

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## I2
+
+[![Build Status](https://travis-ci.org/i-2/i-2.github.io.svg?branch=master)](https://travis-ci.org/i-2/i-2.github.io)
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'sourcepirate'
+SITENAME = 'Infinite intellect'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+
+THEME='themes/cleanblog'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/sourcepirate'),
+          ('github', 'https://github.com/sourcepirate'),
+          ('facebook','https://facebook.com/sathya.shadow'),
+          ('envelope','sathyanarrayanan@yandex.com'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
